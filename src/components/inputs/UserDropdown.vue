123f39bfd5db78f83d658c90ca7a64745bd8f037
Malika Efremova <malikaefremova@gmail.com>
2025-01-26 16:44:29 +0400

    registered user wrapper field as global component and removed its local imports

--- src/components/inputs/UserDropdown.vue
+++ src/components/inputs/UserDropdown.vue
@@ -1,7 +1,3 @@
 <template>
   <user-field-wrapper />
 </template>
-
-<script setup lang="ts">
-import UserFieldWrapper from '@/components/inputs/UserFieldWrapper.vue';
-</script>
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import FormInput from '@/components/forms/FormInput.vue';
 import FormRadio from '@/components/forms/FormRadio.vue';
 import FormDropdown from '@/components/forms/FormDropdown.vue';
 
+import UserFieldWrapper from '@/components/inputs/UserFieldWrapper.vue';
 import UserField from '@/components/inputs/UserField.vue';
 
 createApp(App)
@@ -17,5 +18,6 @@ createApp(App)
   .component('form-input', FormInput)
   .component('form-radio', FormRadio)
   .component('form-dropdown', FormDropdown)
+  .component('user-field-wrapper', UserFieldWrapper)
   .component('user-field', UserField)
   .mount('#app');
