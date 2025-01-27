6730f3fbba537303a2f12e420622547e168dd535
Malika Efremova <malikaefremova@gmail.com>
2025-01-27 14:22:21 +0400

    removed unnecessary comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,14 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    // "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "error",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     'comma-dangle': ['error', 'always-multiline'],
     "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
-    // "vue/no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
   },
 };
